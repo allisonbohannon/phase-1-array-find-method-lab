454718e4c4a41f24a48c05ee9ef2f6a12d3a025b
Allison Bohannon <akboh.github@gmail.com>
2022-04-16 20:37:19 -0700

    commit tree 2e5de7a1557efc62409d73ec70a9614f2e186a7a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(arr, index) {
+     const result = arr.find(item => item.result === 'W')
+     console.log(result)
+     return (result ? result.year : undefined)
+    
+    
+}
+
+
+const record = [
+{ year: "2018", result: "L"},
+{ year: "2014", result: "N/A"},
+{ year: "2013", result: "W"}
+]
+console.log(superbowlWin(record))
